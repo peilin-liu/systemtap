9b9aff7cd059d4c832562716095f6e41aa329872
Martin Cermak <mcermak@redhat.com>
2016-09-06 11:29:15 +0200

    Make multidimensional pmaps work too.
    Make multidimensional pmaps work too.  Fix the calls to
    _stp_pmap_new_ix(), _stp_pmap_new_iix(), ..., _stp_pmap_new_iiiiiiiiix(),
    _stp_pmap_new_six(), _stp_pmap_new_sx(), _stp_pmap_new_ssx() etc.

--- translate.cxx
+++ translate.cxx
@@ -809,7 +809,8 @@ struct mapvar
       throw SEMANTIC_ERROR(_F("unsupported local map init for %s", value().c_str()));
 
     string prefix = "global_set(" + c_name() + ", ";
-    prefix += function_keysym("new") + " (" + lex_cast(sd.stat_ops) + ", "
+    prefix += function_keysym("new") + " ("
+      + ((function_keysym("new").find("_stp_pmap_new") != std::string::npos) ? lex_cast(sd.stat_ops) + ", " : "")
       + (maxsize > 0 ? lex_cast(maxsize) : "MAXMAPENTRIES")
       + ((wrap == true) ? ", 1" : ", 0");
 
